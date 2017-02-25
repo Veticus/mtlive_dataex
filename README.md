19c7369aed1f0fe3ca1e5ce9fdf1d15977760a83
Tim <tim@b-rmbp.local>
2017-02-25 11:26:51 +0100

    Initial commit - No, really...

---
+++ README.md
@@ -0,0 +1,53 @@
+# mtlive-dataex
+
+Allows the usage of the data available on live.midttrafik.dk in other projects.
+
+
+### Prerequisites
+
+To use this python 3.4 or above is needed.
+The code is only tested on Python 3.4 on Linux and on Python 3.5.1 on OSX.
+
+NB: This WILL NOT WORK on windows, in it's current form. This will, however, be changed later.
+
+### Usage
+
+As this is currently in a develop"mental" stage, running the .py file will only show information about the Line 100 of busses operated by Midttrafik. This may be boring. It is possible however to do exactly that, by running the python file as is.
+
+On OSX:
+```
+python3 get_busdata.py
+```
+
+On Linux:
+```
+python get_busdata.py
+```
+
+CLA will be implemented at a later stage.
+
+## Usage of the functions
+
+By importing the get_busdata.py file, you can call the print_businfo_for_line() function.
+It requires a line number to be given. Lines can be found here: [Midttrafik køreplaner](https://www.midttrafik.dk/koereplaner.aspx)
+
+## Built With
+
+* [Jetbrains PyCharm](https://www.jetbrains.com/pycharm/) - The IDE used. It's amazing!
+
+
+
+## Authors
+
+* **Tim Engel** - *Initial work* - [Veticus](https://github.com/Veticus)
+* **Emil Madsen** - *xpath magician* - [Skeen](https://github.com/Skeen)
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
+
+## Acknowledgments
+
+* I owe [Emil 'Skeen' Madsen](https://github.com/Skeen) a Club Mate, for helping me with the xpath.
+* I owe [Mark 'Robotto' Moore](https://github.com/Robotto)
+* I owe JetBrains a collossal hug for allowing students the usage of their products free of charge <3
---
+++ get_busdata.py
@@ -0,0 +1,66 @@
+# coding=utf-8
+# !/usr/bin/python
+
+import urllib.request as rq
+from xml.etree import ElementTree as Et
+
+__project__ = "mtlive-dataex"
+__filename__ = "get_busdata.py"
+__author__ = "Tim Engel"
+__date__ = "25/02/2017"
+__using__ = "PyCharm"
+__interpreter_version__ = "3.5.1"
+__version__ = "0.0.1"
+
+#  These are just some "safe values", aimed to gather as many busses as possible.
+#  TODO: Randomize these everytime data is requested
+lon = 10.031171035766636
+lat = 57.71612970581591
+rad = 463316.96416736324
+getbussesurl = "https://live.midttrafik.dk/getbuses.php?lat={}&lon={}&radius={}".format(str(lat), str(lon), str(rad))
+usedummydata = False  # Set to false, this lets the program request the data from the online service.
+
+
+def allbusdata(live=False):
+	if not usedummydata: live = True
+	if not live:
+		# To prevent "flooding" the live.midttrafik.dk service during development
+		# this allows the use of dummy data stored in the "dummy_response" file.
+		busdata = open("dummy_response").read()
+		print("using dummydata")
+		return busdata
+	data_getter = rq.urlopen(getbussesurl)  # Creates an instance of urllib.request and opening the getbusses url.
+	busdata = data_getter.read()  # Throws the entire response into a single string, for later processing.
+	print("using live data")  # TODO: Remove dummydata usage indicator at some point
+	return busdata
+
+
+def getbusdom():
+	# This should return an ElementTree containing all the busdata
+	dom = Et.fromstring(allbusdata(live=False))
+	return dom
+
+
+def print_businfo_for_line(line):
+	# This should print some info about a given line.
+	num = 0
+	xpath = 'Bus[@Line="{}"]'.format(line)  # Here the xpath is assembled.
+	# The xpath is used too identify "rows", where the Line attribute matches whatever the line variable is set to.
+	# Hereafter the .findall() function allows the use of .attrib in the for-loop.
+	for i in getbusdom().findall(xpath):
+		num += 1
+		print(str(num))
+		print("Name:        " + i.attrib['Name'])
+		print("Line:        " + i.attrib['Line'])
+		print("Text:        " + i.attrib['DirectionText'])
+		print("End name:    " + i.attrib['EndName'])
+		print("Start name:  " + i.attrib['StartName'])
+		# TODO: Add a "current time" thingy and a "time since last update" thingy
+		print("Updated:     " + i.attrib['Updated'])
+		print("Delay:       " + i.attrib['Delay'])
+		print("Start time:  " + i.attrib['StartTime'])
+		print("End time:    " + i.attrib['EndTime'])
+		print("-------------------------------------")
+	print("There are currently {} busses of line {} on the road.".format(num, line))
+
+print_businfo_for_line(100)  # For shits, giggles and testing this will use line 100 for testing.
